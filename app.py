985d8c8dd001df36d44938473ad64b7888733462
Marcello Prado Muller <marcellopradomuller@gmail.com>
2025-12-03 18:39:48 -0300

    ajustando app.py e add gunicorn no requirements

--- app.py
+++ app.py
@@ -46,8 +46,6 @@ def projeto_detalhe(id):
 
     return render_template("projetoViews.html", projeto=projeto)
 
-if __name__ == '__main__':
-    app.run(debug=True)
     
     #^ Para inicializar o ambiente virtual - venv\Scripts\activate
 #^  Arquivo principal do Flask para rodar o portfólio - python app.py
